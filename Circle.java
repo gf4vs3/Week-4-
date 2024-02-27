f6cf6906b34d09abd823f6315c03b4bec8ac3b05
gf4vs3 <137363372+gf4vs3@users.noreply.github.com>
2024-02-27 14:39:58 -0500

    rthgrtrt

---
+++ Circle.java
@@ -0,0 +1,32 @@
+
+
+/*************************************
+ *Project:  Alexander Zalewski 
+ *Date:  2/8
+ *Program Name:  Circle.java
+**************************************/ 
+
+public class Circle{
+
+    public static void main (String[] args){
+        double radius = 4.28; 
+        double circumference = 0; 
+        double area = 0; 
+        
+        circumference = 2*3.14*radius; 
+        area = 3.14*radius*radius;
+
+       
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ Rectangle.java
@@ -0,0 +1,39 @@
+
+
+/*************************************
+ *Project:  Alexander Zalewski 
+ *Date:  2/8
+ *Program Name:  Rectangle.java \b \t \n \f \r \ \' \\ 
+**************************************/ 
+
+public class Rectangle{
+
+    public static void main (String[] args){
+        System.out.println("\n*****************");
+        System.out.println("\t\t*\r*");
+        System.out.println("\t\t*\r*");
+        System.out.println("\t\t*\r*");
+        System.out.println("\t\t*\r*");
+        System.out.println("\t\t*\r*");
+        System.out.println("\t\t*\r*");
+        System.out.println("\t\t*\r*");
+        System.out.println("\b*****************");
+
+
+       
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
