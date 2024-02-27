8f6c1574978efdc9a883f6b20144414220876bd1
gf4vs3 <137363372+gf4vs3@users.noreply.github.com>
2024-02-27 14:55:01 -0500

    Create Digits.java

---
+++ Digits.java
@@ -0,0 +1,20 @@
+public class Digits 
+{
+   public static void main(String[] args) 
+   {
+      
+      int hundred;
+      char ten;
+      double one;
+
+      
+      hundred = 2;
+      ten = '5';
+      one = 8;
+
+      
+      System.out.println(hundred);
+      System.out.println(ten);
+      System.out.println(one);
+   }
+} 
